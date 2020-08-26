c92fecb7ac20f172dfc42a665867f6a1e8b14c8e
Sharkaboi <sarathsedu@gmail.com>
2020-08-26 19:18:43 +0530

    initial commit

---
+++ app/src/main/java/com/cybershark/drawingsapp/DrawingsApp.kt
@@ -0,0 +1,7 @@
+package com.cybershark.drawingsapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DrawingsApp : Application()
---
+++ app/src/main/java/com/cybershark/drawingsapp/ui/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.cybershark.drawingsapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cybershark.drawingsapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
